0d27a60a86f1c0dde8198a20481ca644dfb888bf
john <johnsantosh2@gmail.com>
2023-02-26 17:35:03 +0530

    refactor done , fully update

--- script/index.js
+++ script/index.js
@@ -1,36 +1,5 @@
-const removeFromArray = function(src,...rmv) {
-    // using for loop for checking existence of the element in src 
-    for(let i = 0 ; i<rmv.length ; i++){ 
-        if(src.includes(rmv[i])){ 
-            // getting index of the elements to be removed 
-            let a = src.indexOf(rmv[i]) 
-            // removing elements from src (the parameter)
-            src.splice(a,1) 
-            };
-        } 
-        return src
-}
+let dError= document.getElementById('error')
 
-// console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3))
-
-const sumAll = function(start, end) {
-    let result = 0 ;
-    // checking whether the inputs are Number only
-    if(start !== Number(start) || end !== Number(end)  ) return "ERROR" ;
-    // checking for negative values
-    if(start <0 || end < 0 ) return "ERROR"
-
-    // swapping value if start is greater than end 
-    if(start > end ){
-        let temp = start ;
-        start = end ;
-        end  = temp
-    }
-    // adding each value using for loop and storing it to result
-    for (let i = start ; i<=end ; i++){
-        result+=i
-    }
-    return result;
-};
-
-console.log(sumAll(-10, 1))
+dError.addEventListener('click',function (){
+alert("Sorry! something went wrong")
+})
