59ede13d8a6b52ff1d5617343c120c94cc2b43ad
john <johnsantosh2@gmail.com>
2023-03-18 20:14:49 +0530

    fixed some UI issues

--- script/index.js
+++ script/index.js
@@ -1,5 +1,19 @@
-let dError= document.getElementById('error')
+let displayError = document.getElementById("error");
 
-dError.addEventListener('click',function (){
-alert("Sorry! something went wrong")
-})
+displayError.addEventListener("click", function (e) {
+  e.preventDefault();
+  setTimeout(() => {
+    alert("Sorry! something went wrong");
+  }, 1500);
+});
+
+const anime = document.querySelector(".find-more div img");
+
+anime.addEventListener("mouseenter", () => {
+  console.log(123);
+  anime.style.animation = "fadeInOut 0.5s linear";
+  // setTimeout((anime.style.animation = ""), 5000);
+  setTimeout(() => {
+    anime.style.animation = "";
+  }, 1500);
+});
